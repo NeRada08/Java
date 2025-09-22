a5a7be06d8f3a9fe160a991193678c897df84664
NeRada08 <rada.shumskaya@gmail.com>
2025-09-22 11:42:19 +0300

    Добавила лабу 1, переименовав активити.

--- app/src/main/java/com/example/myapplication/MondayCountActivity.java
+++ app/src/main/java/com/example/myapplication/MondayCountActivity.java
@@ -1,6 +1,5 @@
 package com.example.myapplication;
 
-import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -9,13 +8,10 @@ import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 import java.util.Calendar;
 
-public class MainActivity extends AppCompatActivity {
+public class MondayCountActivity extends AppCompatActivity {
     private Button pickDateBtn;
     private DatePicker datePicker;
     private TextView textView;
@@ -25,7 +21,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.monday_count_activity);
 
 
 
